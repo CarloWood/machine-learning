74bfb5142244a63c09057ec069f506c4bf8003ca
Carlo Wood <carlo@alinoe.com>
2023-11-13 21:04:04 +0100

    Style changes.

--- src/basic-graphdef.cxx
+++ src/basic-graphdef.cxx
@@ -1,9 +1,9 @@
 #include <tensorflow/cc/ops/standard_ops.h>
 #include <tensorflow/core/framework/tensor.h>
 #include <tensorflow/core/public/session.h>
+#include <iostream>
 
-// So far in other examples we have executed the session
-// using the scope.
+// So far in other examples we have executed the session using the scope.
 //
 // There is an another way of performing the same operation and that is
 // by creating the GraphDef object. This method is interesting as with this
@@ -14,51 +14,52 @@
 // default graph. It is being implicitly passed to all APIs in the same
 // context (scope).
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
   using namespace tensorflow;
   using namespace tensorflow::ops;
 
   {
-    // create a root scope
+    // Create a root scope.
     auto scope = Scope::NewRootScope();
 
     auto a = Const(scope.WithOpName("A"), 23);
     auto b = Const(scope.WithOpName("B"), 2);
 
-    // here we are creating two ops (Add & Sub)
+    // Here we are creating two ops (Add & Sub).
     // Add - a + b
     // Sub - Result of (a+b) - b
     //
-    // Here you can see that Sub requires Add operation
+    // Here you can see that Sub requires Add operation.
     auto add = Add(scope.WithOpName("Add"), a, b);
     auto sub = Sub(scope.WithOpName("Sub"), add, b);
 
-    // essentially we are are getting the default graph
-    // that gets created by the framework for the root scope
+    // Essentially we are are getting the default graph
+    // that gets created by the framework for the root scope.
     GraphDef graph;
     TF_CHECK_OK(scope.ToGraphDef(&graph));
 
-    // so far in other examples we have used ClientSession
+    // So far in other examples we have used ClientSession
     // here is another way to create the session object.
     //
     // While ClientSession is used to execute graph created with in
     // the C++ code, it is desirable (most of the time) to load the
     // serialized graph.
     //
-    // Note that in this example we are still creating the grpah in C++. In
-    // other words not loading from a file.
+    // Note that in this example we are still creating the grpah in C++.
+    // In other words not loading from a file.
     std::unique_ptr<tensorflow::Session> session(
         tensorflow::NewSession(tensorflow::SessionOptions()));
 
-    // we are binding the session to this graph
+    // We are binding the session to this graph.
     TF_CHECK_OK(session->Create(graph));
 
     std::vector<Tensor> outputs;
 
     // This is where you will see differences between ClientSession & Session
-    // more clearly
+    // more clearly.
     //
-    // The Run method of Session is quite different from that of ClientSession
+    // The Run method of Session is quite different from that of ClientSession.
     //
     // Here instead of providing the final node of the constructed graph as the
     // input we would supply the Tensor values as the input. To be more clear,
@@ -66,9 +67,9 @@ int main(int argc, char** argv) {
     // corresponing value. That said, in this particular example the inputs (A &
     // B) are constants so we do not need to specify them.
     //
-    // This is why the first argument of Run is {}
+    // This is why the first argument of Run is {}.
     // The second argument is the names of the output nodes that you want to
-    // extract
+    // extract.
 
     TF_CHECK_OK(session->Run({}, {"Sub"}, {}, &outputs));
 
@@ -79,7 +80,7 @@ int main(int argc, char** argv) {
     // What if we wanted the outputs of Add & Sub. Note that Add is not
     // necessarily the last node in our graph but it is still possible to
     // extract the value of A node. Simply specify the names of the output nodes
-    // that you want
+    // that you want.
 
     outputs.clear();
 
@@ -91,9 +92,9 @@ int main(int argc, char** argv) {
   }
 
   {
-    // this is very similar example as above but in this case
+    // This is very similar example as above but in this case
     // we intend to also supply the values of the inputs i.e. not use
-    // Consts but Placeholders
+    // Consts but Placeholders.
 
     auto scope = Scope::NewRootScope();
 
@@ -108,25 +109,25 @@ int main(int argc, char** argv) {
     std::unique_ptr<tensorflow::Session> session(
         tensorflow::NewSession(tensorflow::SessionOptions()));
 
-    // we are binding the session to this graph
+    // We are binding the session to this graph.
     TF_CHECK_OK(session->Create(graph));
 
     std::vector<Tensor> outputs;
 
-    // create two tensors
+    // Create two tensors.
     auto inputA = Tensor(DT_INT32, TensorShape({}));
     auto inputB = Tensor(DT_INT32, TensorShape({}));
 
-    // set their values
+    // Set their values.
     inputA.scalar<int>()() = 10;
     inputB.scalar<int>()() = 20;
 
-    // This is where you would want to pay attention to
+    // This is where you would want to pay attention to.
     // Here we are specifying the inputs using the name
-    // of the nodes (see how Placeholders were created)
+    // of the nodes (see how Placeholders were created).
     std::vector<std::pair<string, tensorflow::Tensor>> inputs = {
-        {"A", inputA},
-        {"B", inputB},
+      {"A", inputA},
+      {"B", inputB},
     };
 
     TF_CHECK_OK(session->Run(inputs, {"Add"}, {}, &outputs));
@@ -134,6 +135,4 @@ int main(int argc, char** argv) {
     std::cout << "Size of outputs -> " << outputs.size() << std::endl;
     std::cout << "DebugString Add -> " << outputs[0].DebugString() << std::endl;
   }
-
-  return 0;
 }
--- src/basic-tensors.cxx
+++ src/basic-tensors.cxx
@@ -1,45 +1,43 @@
 #include <tensorflow/cc/client/client_session.h>
 #include <tensorflow/cc/ops/standard_ops.h>
 #include <tensorflow/core/framework/tensor.h>
+#include <iostream>
 
-// Basic tensor operations using tensorflow
+// Basic tensor operations using tensorflow.
 
 // The sample demonstrates how to
-// - create various operations
-// - use placeholder and placeholderWithDefault
-// - use different overloads of Run method
+// - create various operations.
+// - use placeholder and placeholderWithDefault.
+// - use different overloads of Run method.
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv)
+{
   using namespace tensorflow;
   using namespace tensorflow::ops;
 
-  // create a root scope
+  // Create a root scope.
   auto scope = Scope::NewRootScope();
 
   {
-    // we already know how to create Scalar
-    // which is a tensor of Rank 0
+    // We already know how to create Scalar which is a tensor of Rank 0.
     auto aScalar = Input(2);
-    std::cout << "Dimensions of a scalar - " << aScalar.tensor().shape().dims()
-              << std::endl;
+    std::cout << "Dimensions of a scalar - " << aScalar.tensor().shape().dims() << std::endl;
 
-    // A tensor of Rank 1 is called a vector
+    // A tensor of Rank 1 is called a vector.
     auto aVector = Input({2, 3});
-    std::cout << "Dimensions of a vector - " << aVector.tensor().shape().dims()
-              << std::endl;
+    std::cout << "Dimensions of a vector - " << aVector.tensor().shape().dims() << std::endl;
 
-    // A tensor of Rank 2 is called a matrix
+    // A tensor of Rank 2 is called a matrix.
     auto aMatrix = Input({{2, 3}, {6, 5}});
-    std::cout << "Dimensions of a matrix - " << aMatrix.tensor().shape().dims()
-              << std::endl;
+    std::cout << "Dimensions of a matrix - " << aMatrix.tensor().shape().dims() << std::endl;
 
-    // A tensor of Rank 3 or more is not known by any special name tensor
+    // A tensor of Rank 3 or more is not known by any special name tensor.
   }
 
   {
-    // When building the ops you can specify the shape explicitly as well
+    // When building the ops you can specify the shape explicitly as well.
 
-    // 2x2 matrix with all elements = 10
+    // 2x2 matrix with all elements = 10.
     auto c1 = Const(scope, 10, /* shape */ {2, 2});
 
     // [1 1] * [41; 1]
@@ -53,8 +51,7 @@ int main(int argc, char **argv) {
 
     TF_CHECK_OK(status);
 
-    std::cout << "Underlying Scalar value -> " << outputs[0].flat<int>()
-              << std::endl;
+    std::cout << "Underlying Scalar value -> " << outputs[0].flat<int>() << std::endl;
 
     // [1 2 3 4] + 10
     auto y = Add(scope, {1, 2, 3, 4}, 10);
@@ -63,9 +60,6 @@ int main(int argc, char **argv) {
 
     TF_CHECK_OK(status);
 
-    std::cout << "Underlying vector value -> " << outputs[0].flat<int>()
-              << std::endl;
+    std::cout << "Underlying vector value -> " << outputs[0].flat<int>() << std::endl;
   }
-
-  return 0;
 }
