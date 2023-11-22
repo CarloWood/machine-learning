37484ecf0c1a672cc7947548f3d68563106b0b00
Carlo Wood <carlo@alinoe.com>
2023-11-22 21:20:00 +0100

    Add DebugShow. Use WX instead of XW.

--- src/neural_network.cxx
+++ src/neural_network.cxx
@@ -7,38 +7,6 @@
 using namespace tensorflow;
 using namespace tensorflow::ops;
 
-// Function to create a dense (fully connected) layer.
-Output SigmoidDenseLayer(Scope& scope, Input inputs, Input labels, int input_units, int output_units, std::vector<Operation>& need_running,
-    std::vector<Output>* debug_outputs)
-{
-  // Define a weights matrix.
-  auto weights = Variable(scope, {output_units, input_units}, DT_FLOAT);
-
-  // Initialization of the weights.
-  need_running.push_back(Assign(scope, weights, RandomNormal(scope, {output_units, input_units}, DT_FLOAT)).operation);
-
-  // Define a biases vector.
-  auto biases = Variable(scope, {output_units}, DT_FLOAT);
-
-  // Initialization of the biases.
-  need_running.push_back(Assign(scope, biases, RandomNormal(scope, {output_units}, DT_FLOAT)).operation);
-
-  // Make a copy of the weights and biases for inspection.
-  if (debug_outputs)
-  {
-    debug_outputs->push_back(Identity(scope, weights));
-    debug_outputs->push_back(Identity(scope, biases));
-  }
-
-  // Perform `weights * inputs + biases` and apply activation function.
-  auto output = Sigmoid(scope, Add(scope, MatMul(scope, weights, inputs), biases));
-
-  // Define a loss function, lets use Mean Squared Error (MSE) (totally random).
-  auto loss = ReduceMean(scope, Square(scope, Subtract(scope, labels, output)), {0});
-
-  return output;
-}
-
 void dump_graph(Scope const& scope, std::string name)
 {
   // Create a GraphDef object to hold the graph.
@@ -58,59 +26,115 @@ void dump_graph(Scope const& scope, std::string name)
 
 constexpr int UnknownRank = -1;
 
-float y(float x)
+float x1(float x0)
 {
-  return 2.5 * x - 4.3;
+  return 2.5f * x0 - 4.3f;
 }
+
+class DebugShow
+{
+ private:
+  Scope& scope_;
+  std::vector<std::string> labels_;
+  std::vector<Output> fetch_outputs_;
+  std::vector<Tensor> outputs_;
+
+ public:
+  DebugShow(Scope& scope) : scope_(scope) { }
+
+  std::vector<Output> const& fetch_outputs() const { return fetch_outputs_; }
+  std::vector<Tensor>* outputs() { return &outputs_; }
+
+  void operator()(std::string label, Output const& output)
+  {
+    labels_.push_back(label);
+    fetch_outputs_.push_back(Identity(scope_, output));
+  }
+
+  void dump() const
+  {
+    for (int i = 0; i < labels_.size(); ++i)
+      std::cout << labels_[i] << ": " << outputs_[i].DebugString(40) << std::endl;
+  }
+};
 
 int main()
 {
   // Define the scope.
   Scope scope = Scope::NewRootScope();
+  DebugShow debug_show(scope);
 
   //---------------------------------------------------------------------------
   // Create graph.
 
-  // Define the input placeholder with shape [UnknownRank, 2], where 'UnknownRank' means the first dimension is dynamic.
-  auto inputs = Placeholder(scope.WithOpName("inputs"), DT_FLOAT, Placeholder::Shape({UnknownRank, 2}));
+  // Define the input placeholder with shape [2, UnknownRank], where 'UnknownRank' means the first dimension is dynamic.
+  auto inputs = Placeholder(scope.WithOpName("inputs"), DT_FLOAT, Placeholder::Shape({2, UnknownRank}));
+  debug_show("inputs", inputs);
+
   // Define the labels placeholder with shape [UnknownRank], where 'UnknownRank' means the dimension is dynamic.
   auto labels = Placeholder(scope.WithOpName("labels"), DT_FLOAT, Placeholder::Shape({UnknownRank}));
+  debug_show("labels", labels);
 
-  //        .---.
-  // x ---> |   | ---> [tanh activation] ---> { >0 --> label = 1.
-  // y ---> |   |                             { <0 --> label = 0.
-  //        '---'
+  //         .---.
+  // x₀ ---> |   | ---> [tanh activation] ---> { >0 --> label = 1.
+  // x₁ ---> |   |                             { <0 --> label = 0.
+  //         '---'
   //
-  // v is the input vector (x, y), or (x1, x2).
-  // w is the weights vector (w1, w2).
-  // b is the bias, a scalar.
+  // Notation: row: [...], column: <...>, depth: {...}. All indices are in the order row, column, depth.
+  // X is the input column-vector <x₀ x₁ 1>.
+  // W is the weights/bias row-vector [w₀ w₁ b].
   //
-  // Pre-activation output = v ⋅ w + b = x1⋅w1 + x2⋅w2 + b.
+  // Pre-activation output = WX = w₀x₀ + w₁x₁ + b.
   // Since the activation function tanh leaves the sign unchanged, we want the pre-activation output
   // to be zero when a point is exactly on the line, positive when it is above the line, and negative
-  // when it is below the line. This means that w2 must be positive.
+  // when it is below the line. This means that w₁ must be positive.
   //
-  // Using the above y(float x): x2 = 2.5 * x1 - 4.3,
-  // we have: x1⋅w1 + (2.5 * x1 - 4.3)⋅w2 + b = 0, and w2 > 0 -->
-  // x1⋅(w1 + 2.5⋅w2) - 4.3⋅w2 + b = 0 for any x1 -->
-  // w1 + 2.5⋅w2 = 0 and -4.3⋅w2 + b = 0 -->
-  // w1 = -2.5⋅w2, and b = 4.3⋅w2.
-  // For example, if w2=1 > 0, then w1=-2.5 and b=4.3.
+  // Using the above function x1(x0): x₁ = 2.5 x₀ - 4.3,
+  // we have: w₀x₀ + w₁(2.5 x₀ - 4.3) + b = 0 -->
+  // (w₀ + 2.5 w₁)x₀ - 4.3⋅w₁ + b = 0 for any x₀ -->
+  // w₀ + 2.5⋅w₁ = 0 and -4.3⋅w₁ + b = 0 -->
+  // w₀ = -2.5⋅w₁, and b = 4.3⋅w₁.
+  // For example, if w₁=1 > 0, then w₀=-2.5 and b=4.3.
   //
   // This proves that a single perceptron with two inputs is able to learn
   // how to separate 2D points from laying above or below a line.
 
   // Create a neural network layer existing of a single perceptron with two inputs and one output.
   std::vector<Operation> initialization_operations;
-  std::vector<Output> debug_outputs;
-  auto output = SigmoidDenseLayer(scope, inputs, labels, 2, 1, initialization_operations, &debug_outputs);
+  int input_units = 2;
+  int output_units = 1;
 
-  // Gradients of loss w.r.t weights and biases
-//  std::vector<tensorflow::Output> grad_outputs;
-//  TF_CHECK_OK(AddSymbolicGradients(scope, {loss}, {weights, biases}, &grad_outputs));
+  // Define a weights_bias matrix.
+  auto weights_bias = Variable(scope, {output_units, input_units + 1}, DT_FLOAT);
 
-  // Assuming you have a learning rate defined as a constant or placeholder.
-//  auto optimizer = ApplyGradientDescent(scope, /* trainable variables */, /* learning rate */, /* gradient computation */);
+  // Initialization of the weights and bias.
+  initialization_operations.push_back(Assign(scope, weights_bias, RandomNormal(scope, {output_units, input_units + 1}, DT_FLOAT)).operation);
+  debug_show("weights_bias", weights_bias);
+
+  // Get the dynamic shape of 'inputs'.
+  auto inputs_shape = Shape(scope, inputs);
+
+  // Extract the batch size (the second dimension of 'inputs_shape')
+  auto batch_size = Slice(scope, inputs_shape, {1}, {1});
+
+  // Create a tensor of ones with dynamic shape [batch_size].
+  auto ones = Fill(scope, batch_size, 1.0f);
+  debug_show("ones", ones);
+
+  // For Concat the work, 'ones' needs to have the shape [batch_size, 1].
+  auto ones_expanded = ExpandDims(scope, ones, 0);
+  debug_show("ones_expanded", ones_expanded);
+
+  // Concatenate 'inputs' and 'ones_expanded' along the second dimension.
+  auto inputs_1 = Concat(scope, {Input(inputs), ones_expanded}, 0);
+  debug_show("inputs_1", inputs_1);
+
+  // Perform `weights_bias * inputs_1` and apply activation function.
+  auto output = Sigmoid(scope, MatMul(scope, weights_bias, inputs_1));
+  debug_show("output", output);
+
+  // Define a loss function, lets use Mean Squared Error (MSE) (totally random).
+  auto loss = ReduceMean(scope, Square(scope, Subtract(scope, output, labels)), {0});
 
   // Write the graph to a file.
   dump_graph(scope, "neural_network");
@@ -124,26 +148,25 @@ int main()
 
   std::vector<float> labels_data = { 0, 1, 1, 0, 0, 1, 0, 0, 1 };
   int64 const number_of_data_points = labels_data.size();
-  std::vector<float> inputs_data;
+
+  // Create a tensor with labels.
+  Tensor labels_tensor{DT_FLOAT, TensorShape{number_of_data_points}};
+  std::copy_n(labels_data.begin(), labels_data.size(), labels_tensor.flat<float>().data());
+
+  // Create a tensor with inputs.
+  Tensor inputs_tensor{DT_FLOAT, TensorShape{2, number_of_data_points}};
+  auto tensor_map = inputs_tensor.tensor<float, 2>();
   for (int i = 0; i < number_of_data_points; ++i)
   {
-    float x = i;
-    float delta_y = (labels_data[i] == 1 ? 1 : -1) * distribution(generator);
+    float x0 = i;
+    float delta_x1 = (labels_data[i] == 1 ? 1 : -1) * distribution(generator);
 
-    inputs_data.push_back(x);
-    inputs_data.push_back(y(x) + delta_y);
+    tensor_map(0, i) = x0;
+    tensor_map(1, i) = x1(x0) + delta_x1;
   }
 
-  // Create a tensor for inputs and labels.
-  Tensor inputs_tensor{DT_FLOAT, TensorShape{number_of_data_points, 2}};
-  Tensor labels_tensor{DT_FLOAT, TensorShape{number_of_data_points}};
-
-  // Fill the tensors with our training data.
-  std::copy_n(labels_data.begin(), labels_data.size(), labels_tensor.flat<float>().data());
-  std::copy_n(inputs_data.begin(), inputs_data.size(), inputs_tensor.flat<float>().data());
-
   std::cout << "labels_tensor is now " << labels_tensor.DebugString(number_of_data_points) << std::endl;
-  std::cout << "inputs_tensor is now " << inputs_tensor.DebugString(number_of_data_points * 2) << std::endl;
+  std::cout << "inputs_tensor is now " << inputs_tensor.DebugString(2 * number_of_data_points) << std::endl;
 
   //---------------------------------------------------------------------------
   // Running.
@@ -153,13 +176,14 @@ int main()
   // Fill weights and biases with random initial values.
   TF_CHECK_OK(session.Run({}, {}, initialization_operations, nullptr));
 
-  // Read back and print debug outputs (initial values of weights and biases).
-  std::vector<Tensor> outputs;
-  TF_CHECK_OK(session.Run({}, debug_outputs, {}, &outputs));
-  std::cout << "Weights: " << outputs[0].DebugString() << std::endl;
-  std::cout << "Biases: " << outputs[1].DebugString() << std::endl;
+  // Associate the placeholder with the input data.
+  ClientSession::FeedType inputs_feed = {
+    {inputs, inputs_tensor},
+    {labels, labels_tensor}
+  };
+  TF_CHECK_OK(session.Run(inputs_feed, debug_show.fetch_outputs(), debug_show.outputs()));
+  debug_show.dump();
 
   //---------------------------------------------------------------------------
   // Training.
-
 }
