48b09dae85ccd5dbd21379f7812c70f30b60c146
Carlo Wood <carlo@alinoe.com>
2023-12-17 19:38:31 +0100

    WIP

--- src/cairo_window.cxx
+++ src/cairo_window.cxx
@@ -1,6 +1,8 @@
 #include "sys.h"
 #include "cairowindow/Window.h"
 #include "cairowindow/Layer.h"
+#include "cairowindow/draw/Rectangle.h"
+#include "cairowindow/draw/Line.h"
 #include "utils/AIAlert.h"
 #include "utils/debug_ostream_operators.h"
 #include <thread>
@@ -23,38 +25,25 @@ int main()
     // Open window, handle event loop and block until the window is closed.
     EventLoop event_loop = window.run();
 
-    // Create a new layer with a white background.
-    auto background_layer = window.create_background_layer<Layer>(Color{1, 1, 1});
+    // Create a new layer with a gray background.
+    auto background_layer = window.create_background_layer<Layer>(color::gray);
 
     // Create another layer.
     auto second_layer = window.create_layer<Layer>(Rectangle{100, 100, 600, 400});
 
-    auto green_square = second_layer->create_layer_region<LayerRegion>();
-    green_square->draw([](cairo_t* cr) -> Rectangle {
-      cairo_set_source_rgb(cr, 0, 255, 0); // Green color for drawing.
-      cairo_set_line_width(cr, 2);
-      cairo_move_to(cr, 350, 250);
-      cairo_line_to(cr, 350, 350);
-      cairo_line_to(cr, 450, 350);
-      cairo_line_to(cr, 450, 250);
-      cairo_line_to(cr, 350, 250);
-      cairo_stroke(cr);
-      return {349, 249, 102, 102};
-    });
+    // It doesn't really matter if you destroy the draw::Rectangle afterwards, at the moment.
+    {
+      auto green_square = second_layer->create_layer_region<draw::Rectangle>(Rectangle{350, 250, 100, 100}, color::green);
+      green_square->draw();
+    }
 
     // Draw something on the background layer.
-    auto red_square = background_layer->create_layer_region<LayerRegion>();
-    red_square->draw([](cairo_t* cr) -> Rectangle {
-      cairo_set_source_rgb(cr, 255, 0, 0); // Red color for drawing.
-      cairo_set_line_width(cr, 2);
-      cairo_move_to(cr, 50, 50);
-      cairo_line_to(cr, 400, 50);
-      cairo_line_to(cr, 400, 300);
-      cairo_line_to(cr, 50, 300);
-      cairo_line_to(cr, 50, 50);
-      cairo_stroke(cr);
-      return {49, 49, 352, 252};    // This area includes the line width.
-    });
+    auto red_square = background_layer->create_layer_region<draw::Rectangle>(Rectangle{50, 50, 350, 250}, color::red);
+    red_square->draw();
+
+    // Draw a line.
+    auto blue_line = second_layer->create_layer_region<draw::Line>(Rectangle{350, 250, 100, 100}, color::blue);
+    blue_line->draw();
 
     // Wait for the event loop thread to be finished (the window closed).
     event_loop.set_cleanly_terminated();
