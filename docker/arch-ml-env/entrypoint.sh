c29444b4388eed53a3b31f24eeb669e49173cc6c
Carlo Wood <carlo@alinoe.com>
2024-06-14 14:51:35 +0200

    Correctly initialize /home/archuser/machine-learning

--- docker/arch-ml-env/entrypoint.sh
+++ docker/arch-ml-env/entrypoint.sh
@@ -4,7 +4,8 @@
 set -e
 
 # Function to change ownership if the directory $1 is empty and not already owned by $2:$3.
-fix_ownership_if_empty_and_not_owned() {
+# In that case it is initialize with the default files.
+set_up_initial_machine_learning_dir() {
   local dir=$1
   local uid=$2
   local gid=$3
@@ -20,13 +21,17 @@ fix_ownership_if_empty_and_not_owned() {
       if [ ! "$(ls -A $dir)" ]; then
         echo "Directory $dir is empty and not owned, changing ownership to $uid:$gid."
         sudo chown $uid:$gid $dir
+        # Also copy the initial env.source into its place.
+        cp /usr/local/share/machine-learning/ml-env.source /home/archuser/machine-learning/env.source
+        source /home/archuser/machine-learning/env.source
+        mkdir "$GITACHE_ROOT"
       fi
     fi
   fi
 }
 
-# Fix ownership of /home/archuser/machine-learning.
-fix_ownership_if_empty_and_not_owned /home/archuser/machine-learning $(id -u) $(id -g)
+# Fix ownership of /home/archuser/machine-learning and initialize it.
+set_up_initial_machine_learning_dir /home/archuser/machine-learning $(id -u) $(id -g)
 
 # Execute the original command.
 exec "$@"
