267f42804ac08de4d62860a1eb988ac7b6b387f2
Carlo Wood <carlo@alinoe.com>
2024-11-13 21:05:51 +0100

    CubicPolynomial::get_roots now works for all cubics.
    Should normally take two iterations and in exceptional cases three.

--- math/CubicPolynomial_get_roots.cpp
+++ math/CubicPolynomial_get_roots.cpp
@@ -2,6 +2,12 @@
 // in order to avoid code duplication in tests that need(ed)
 // to add test code to it.
 
+  static constexpr double sqrt3 = 1.7320508075688773;    // √3
+
+#if defined(CWDEBUG) && defined(RESTART)
+  RESTART
+#endif
+
 #ifndef HALLEY_ITERATIONS_TEST
   if (coefficients_[3] == 0.0)
   {
@@ -39,6 +45,16 @@
   double C0, C1;
   double scale;
 
+  // Avoid the local extrema and the inflection point because the derivative might be zero there too.
+  // We go for the root that is the furthest away from the inflection point.
+
+  // Note: the Halley_iterations test defines u itself.
+  double u;
+
+  // If cubic_has_local_extrema then this value is the root when S(C0) = 0,
+  // otherwise it is an approximation of the root for small values of C0.
+  double root0;
+
   if (cubic_has_local_extrema)
   {
     // Transform the cubic into
@@ -67,6 +83,12 @@
     // The applied transform means that any root found must be scaled back by multiplying with
     scale = sqrt_d / 3.0;
     // and then adding Ix back.
+
+    // Define the value of the root in the case that C0 is zero.
+    root0 = std::copysign(sqrt3, -C0);
+
+    // A special case of the initial (and final) guess of the root in the case that C0 is zero.
+    u = root0;
   }
   else
   {
@@ -95,25 +117,15 @@
     // The applied transform means that any root found must be scaled back by multiplying with
     scale = sqrt_md / 3.0;
     // and then adding Ix back.
+
+    // A special case of the initial guess of the root in the case that C0 is zero.
+    u = 0.0;
   }
 #endif  // no HALLEY_ITERATIONS_TEST
   Dout(dc::notice, "C0 = " << std::setprecision(18) << C0 << ", C1 = " << C1);
 
   // Determine if the inflection point is above or below the x-axis.
   bool const inflection_point_y_larger_than_zero = C0 > 0.0;
-
-  static constexpr double sqrt3 = 1.7320508075688773;    // -√3
-  // Define the value of the root in the case that C0 is zero.
-  double root0 = std::copysign(sqrt3, -C0);
-
-  // Avoid the local extrema and the inflection point because the derivative might be zero there too.
-  // We go for the root that is the furthest away from the inflection point.
-
-// the Halley_iterations test defines u itself.
-#ifndef HALLEY_ITERATIONS_TEST
-  // Special case for if zero is a root (i.e. Q(u) = u⋅(u² ± 3)).
-  double u = cubic_has_local_extrema ? root0 : 0.0;
-#endif  // no HALLEY_ITERATIONS_TEST
 
 #ifdef CWDEBUG
 #ifndef RANDOM_CUBICS_TEST
@@ -133,63 +145,72 @@
   }
 #endif
 
-#ifndef HALLEY_ITERATIONS_TEST
+#ifdef HALLEY_ITERATIONS_TEST
+  int max_limit = 100;
+#else
   if (AI_LIKELY(C0 != 0.0))       // Is 0 not a root of the cubic?
   {
+    constexpr int max_limit = 10;
 #endif
-    // We need the cube root of C0.
-    double const cbrtC0 = std::cbrt(C0);
-
-    // Define the value of the root in the case that C0 is large.
-    double const root1 = -(cbrtC0 + 1.0 / cbrtC0);
-
-// the Halley_iterations test defines u itself.
-#ifdef HALLEY_ITERATIONS_TEST
-    int max_limit = 100;
-#else
     double const abs_C0 = std::abs(C0);
-    int max_limit = 10;
-    double SC0_approximation;
-    if (abs_C0 < 6.82)
+    if (!cubic_has_local_extrema && abs_C0 < 2.60987)
+      u = (utils::square(C0) / 81.0 - 1.0 / 3.0) * C0;
+    else
     {
-      if (abs_C0 <= 0.90375741846)
-      {
-        // If cubic_has_local_extrema and |C0| is less than 0.90375741845959156233304814223072905692
-        // then the following second degree polynomial approximates S(C0) such that the maximum
-        // relative error in the guessed root is 0.005.
-        //
-        //    0.2307668111362540090428220 * |C0|   +
-        //    0.3991077472117580786261304 * |C0|^2
-        //
-        // where S(C0) is defined as that `root0 + S(C0)⋅(root1 - root0)` is the exact root;
-        // aka S(C0) = (root - root0) / (root1 - root0).
+      // We need the cube root of C0.
+      double const cbrtC0 = std::cbrt(C0);
 
-        SC0_approximation = (0.230766811136254009 + 0.399107747211758079 * abs_C0) * abs_C0;
-
-        // This polynomial has been determined with `minus_three_case.cxx` with
-        // TILL_C0_half set to 1 (and polynomial_degree = 2).
-      }
+      if (!cubic_has_local_extrema)
+        u = -cbrtC0 + 1.0 / cbrtC0;
       else
       {
-        // If cubic_has_local_extrema and |C0| is larger than 0.90 but less than 6.82
-        // then the following third degree polynomial approximates S(C0) such that the maximum
-        // relative error in the guessed root is 0.0042.
-        //
-        //    0.1336242865900584967672058          +
-        //    0.5309536153375157312519206 * |C0|   +
-        //   -0.1103045467148938868482953 * |C0|^2 +
-        //    0.0074894907819310989348705 * |C0|^3
+        // This value is the root when S(C0) = 1.
+        double const root1 = -(cbrtC0 + 1.0 / cbrtC0);
 
-        SC0_approximation =
-          0.133624286590058497 + (0.530953615337515731 + (-0.110304546714893887 + 0.0074894907819310989 * abs_C0) * abs_C0) * abs_C0;
+#ifndef HALLEY_ITERATIONS_TEST
+        double SC0_approximation;
+        if (abs_C0 < 6.82)
+        {
+          if (abs_C0 <= 0.90375741846)
+          {
+            // If cubic_has_local_extrema and |C0| is less than 0.90375741845959156233304814223072905692
+            // then the following second degree polynomial approximates S(C0) such that the maximum
+            // relative error in the guessed root is 0.005.
+            //
+            //    0.2307668111362540090428220 * |C0|   +
+            //    0.3991077472117580786261304 * |C0|^2
+            //
+            // where S(C0) is defined as that `root0 + S(C0)⋅(root1 - root0)` is the exact root;
+            // aka S(C0) = (root - root0) / (root1 - root0).
 
-        // This polynomial has been determined with `minus_three_case.cxx` with
-        // TILL_C0_half set to 0 (and polynomial_degree = 3).
+            SC0_approximation = (0.230766811136254009 + 0.399107747211758079 * abs_C0) * abs_C0;
+
+            // This polynomial has been determined with `minus_three_case.cxx` with
+            // TILL_C0_half set to 1 (and polynomial_degree = 2).
+          }
+          else
+          {
+            // If cubic_has_local_extrema and |C0| is larger than 0.90 but less than 6.82
+            // then the following third degree polynomial approximates S(C0) such that the maximum
+            // relative error in the guessed root is 0.0042.
+            //
+            //    0.1336242865900584967672058          +
+            //    0.5309536153375157312519206 * |C0|   +
+            //   -0.1103045467148938868482953 * |C0|^2 +
+            //    0.0074894907819310989348705 * |C0|^3
+
+            SC0_approximation =
+              0.133624286590058497 + (0.530953615337515731 + (-0.110304546714893887 + 0.0074894907819310989 * abs_C0) * abs_C0) * abs_C0;
+
+            // This polynomial has been determined with `minus_three_case.cxx` with
+            // TILL_C0_half set to 0 (and polynomial_degree = 3).
+          }
+          u = (1.0 - SC0_approximation) * root0 + SC0_approximation * root1;
+        }
+        else
+          u = root1;
       }
-      u = (1.0 - SC0_approximation) * root0 + SC0_approximation * root1;
     }
-    else
-      u = root1;
 #endif
 
 #ifdef CWDEBUG
@@ -242,11 +263,22 @@
           Qu = next_Qu;
           correct_root = next_root;
         }
-        if (C0 != 0 && C1 != 3) //FIXME: remove the test C1 != 3 as soon as that is implemented.
+        if (C0 != 0)
         {
-          if (!(u == correct_root ||
-              std::nextafter(u, std::numeric_limits<double>::infinity()) == correct_root ||
-              std::nextafter(u, -std::numeric_limits<double>::infinity()) == correct_root))
+          bool close_enough = u == correct_root;
+          if (!close_enough)
+          {
+            double nextafter = std::nextafter(u, std::numeric_limits<double>::infinity());
+            double nextbefore = std::nextafter(u, -std::numeric_limits<double>::infinity());
+            close_enough = nextafter == correct_root || nextbefore == correct_root;
+            if (!close_enough)
+            {
+              nextafter = std::nextafter(nextafter, std::numeric_limits<double>::infinity());
+              nextbefore = std::nextafter(nextbefore, -std::numeric_limits<double>::infinity());
+              close_enough = nextafter == correct_root || nextbefore == correct_root;
+            }
+          }
+          if (!close_enough)
           {
             // Do one more Halley iteration.
             double Q_u = C0 + u * (utils::square(u) + C1);
@@ -254,10 +286,12 @@
             double Qp_u = half_Qpp_u * u + C1;
             u += -Q_u * Qp_u / (utils::square(Qp_u) - Q_u * half_Qpp_u);
 
-            // If now u is within one resolution step from the correct root then we DID stop too soon.
-            ASSERT(!(u == correct_root ||
-              std::nextafter(u, std::numeric_limits<double>::infinity()) == correct_root ||
-              std::nextafter(u, -std::numeric_limits<double>::infinity()) == correct_root));
+            // If now u is within one or two resolution steps from the correct root then we DID stop too soon.
+            double nextafter = std::nextafter(u, std::numeric_limits<double>::infinity());
+            double nextbefore = std::nextafter(u, -std::numeric_limits<double>::infinity());
+            ASSERT(!(u == correct_root || nextafter == correct_root || nextbefore == correct_root ||
+              std::nextafter(nextafter, std::numeric_limits<double>::infinity()) == correct_root ||
+              std::nextafter(nextbefore, -std::numeric_limits<double>::infinity()) == correct_root));
           }
         }
         else
@@ -266,11 +300,13 @@
 #endif
     }
     while (--limit && std::abs(prev_u - u) > std::abs(max_relative_error_before_last_iteration * u));
+#ifdef CWDEBUG
     ASSERT(limit > 0);
     iterations = max_limit - limit;
-
 #ifndef HALLEY_ITERATIONS_TEST
-    ASSERT(C1 != -3.0 || max_limit != 10 || iterations <= 2);
+    // Although very little, it happens that we get three iterations.
+    ASSERT(max_limit != 10 || iterations <= 3);
+#endif
 #endif
 
     Dout(dc::notice, "Root found: " << std::setprecision(18) << u << "; iterations: " << iterations <<
